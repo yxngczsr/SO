24f36840dcf978b18c2316c82261acdc2caa1f6b
César <pedrocesaracc@gmail.com>
2024-05-29 12:34:13 +0100

    update

--- Utils/cp/cp.c
+++ Utils/cp/cp.c
@@ -2,132 +2,159 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <dirent.h>
 #include <string.h>
-#include <limits.h> // Make sure to include this for PATH_MAX
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <limits.h>
 
-#define BUF_SIZE 4096  // Buffer size for file copy
+#define BUF_SIZE 4096
 
-// Function to copy a single file
-int copy_file(const char *src, const char *dest) {
-    int src_fd, dest_fd;
-    ssize_t num_read, num_written;
+void print_options() {
+    printf("\nOptions:\n\n");
+    printf("  <source> <dest>        : Move files or directories.\n");
+    printf("  -r <source> <dest>     : Copy directories recursively.\n");
+    printf("  -i <source> <dest>     : Copy files with confirmation if destination exists.\n");
+    printf("  --help                 : Display this help message.\n\n");
+}
+
+void copy_file(const char *source, const char *dest, int interactive) {
+    int src_fd, dest_fd, nread;
     char buf[BUF_SIZE];
     struct stat src_stat;
 
-    src_fd = open(src, O_RDONLY);
+    src_fd = open(source, O_RDONLY);
     if (src_fd == -1) {
         perror("Failed to open source file");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    if (fstat(src_fd, &src_stat) == -1) {
-        perror("Failed to get source file status");
-        close(src_fd);
-        return -1;
+    fstat(src_fd, &src_stat);
+
+    if (interactive && access(dest, F_OK) == 0) {
+        char response;
+        printf("Destination file '%s' already exists. Overwrite? (y/n): ", dest);
+        scanf(" %c", &response);
+        if (response != 'y' && response != 'Y') {
+            printf("Operation canceled.\n");
+            close(src_fd);
+            exit(EXIT_SUCCESS);
+        }
     }
 
     dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, src_stat.st_mode);
     if (dest_fd == -1) {
-        perror("Failed to open/create destination file");
+        perror("Failed to open destination file");
         close(src_fd);
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    while ((num_read = read(src_fd, buf, BUF_SIZE)) > 0) {
-        char *out_ptr = buf;
-        do {
-            num_written = write(dest_fd, out_ptr, num_read);
-            if (num_written >= 0) {
-                num_read -= num_written;
-                out_ptr += num_written;
-            } else if (errno != EINTR) {
-                perror("Failed to write to destination file");
-                close(src_fd);
-                close(dest_fd);
-                return -1;
-            }
-        } while (num_read > 0);
+    while ((nread = read(src_fd, buf, BUF_SIZE)) > 0) {
+        if (write(dest_fd, buf, nread) != nread) {
+            perror("Failed to write to destination file");
+            close(src_fd);
+            close(dest_fd);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    if (num_read == -1) {
-        perror("Failed to read source file");
-        close(src_fd);
-        close(dest_fd);
-        return -1;
+    if (nread == -1) {
+        perror("Failed to read from source file");
     }
 
     close(src_fd);
     close(dest_fd);
-    return 0;
 }
 
-// Function to recursively copy directories
-int copy_directory(const char *src, const char *dest) {
+void copy_directory(const char *source, const char *dest) {
     DIR *dir;
     struct dirent *entry;
-    struct stat statbuf;
+    struct stat st;
     char src_path[PATH_MAX];
     char dest_path[PATH_MAX];
 
-    if (mkdir(dest, 0777) != 0 && errno != EEXIST) {
-        perror("Failed to create destination directory");
-        return -1;
+    if ((dir = opendir(source)) == NULL) {
+        perror("Failed to open source directory");
+        exit(EXIT_FAILURE);
     }
 
-    dir = opendir(src);
-    if (!dir) {
-        perror("Failed to open source directory");
-        return -1;
+    if (mkdir(dest, 0777) == -1) {
+        perror("Failed to create destination directory");
+        closedir(dir);
+        exit(EXIT_FAILURE);
     }
 
     while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-            continue;
-
-        snprintf(src_path, sizeof(src_path), "%s/%s", src, entry->d_name);
-        snprintf(dest_path, sizeof(dest_path), "%s/%s", dest, entry->d_name);
-
-        if (stat(src_path, &statbuf) == -1) {
-            perror("Failed to get file status");
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
         }
 
-        if (S_ISDIR(statbuf.st_mode)) {
-            if (copy_directory(src_path, dest_path) != 0) {
-                closedir(dir);
-                return -1;
-            }
+        snprintf(src_path, sizeof(src_path), "%s/%s", source, entry->d_name);
+        snprintf(dest_path, sizeof(dest_path), "%s/%s", dest, entry->d_name);
+
+        if (lstat(src_path, &st) == -1) {
+            perror("Failed to get file status");
+            closedir(dir);
+            exit(EXIT_FAILURE);
+        }
+
+        if (S_ISDIR(st.st_mode)) {
+            copy_directory(src_path, dest_path);
         } else {
-            if (copy_file(src_path, dest_path) != 0) {
-                closedir(dir);
-                return -1;
-            }
+            copy_file(src_path, dest_path, 0);
         }
     }
 
     closedir(dir);
-    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    struct stat statbuf;
+    int opt;
+    int recursive = 0, interactive = 0;
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <source> <destination>\n", argv[0]);
+    if (argc < 3) {
+        print_options();
         return EXIT_FAILURE;
     }
 
-    if (stat(argv[1], &statbuf) == -1) {
+    while ((opt = getopt(argc, argv, "ri")) != -1) {
+        switch (opt) {
+            case 'r':
+                recursive = 1;
+                break;
+            case 'i':
+                interactive = 1;
+                break;
+            default:
+                print_options();
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (argc - optind != 2) {
+        fprintf(stderr, "Invalid number of arguments.\n");
+        return EXIT_FAILURE;
+    }
+
+    const char *source = argv[optind];
+    const char *dest = argv[optind + 1];
+
+    struct stat st;
+    if (stat(source, &st) == -1) {
         perror("Failed to get source status");
         return EXIT_FAILURE;
     }
 
-    if (S_ISDIR(statbuf.st_mode)) {
-        return copy_directory(argv[1], argv[2]);
+    if (S_ISDIR(st.st_mode)) {
+        if (recursive) {
+            copy_directory(source, dest);
+        } else {
+            fprintf(stderr, "Source is a directory, use -r option to copy recursively.\n");
+            return EXIT_FAILURE;
+        }
     } else {
-        return copy_file(argv[1], argv[2]);
+        copy_file(source, dest, interactive);
     }
+
+    return EXIT_SUCCESS;
 }
